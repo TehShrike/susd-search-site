eb69c8a670d6be5d5311f252f071f4dbc9114aa6
Josh Duff <me@JoshDuff.com>
2016-11-19 15:43:32 -0600

    Dropping all image downloading, that's in susd-image-mirror now

--- server/download-cache.js
+++ server/download-cache.js
@@ -3,7 +3,7 @@ const createCache = require('levelup-cache')
 const ms = require('ms')
 const nodeify = require('then-nodeify')
 const denodeify = require('then-denodeify')
-const got = require('got')
+const download = require('download')
 const pageParser = require('susd-page-parser')
 const pMap = require('p-map')
 
@@ -16,33 +16,27 @@ const addresses = {
 	game: 'https://www.shutupandsitdown.com/games-page/',
 }
 
-module.exports = function createDownloadingCache({ imagePaths, imageDownloaders }) {
-	const cache = createCache(db, nodeify(downloadDataAndImages), {
+module.exports = function createDownloadingCache({ imageUrlPrefix }) {
+	function stripPrefix(url) {
+		return url.substring(imageUrlPrefix.length)
+	}
+
+	const cache = createCache(db, nodeify(async function downloadData(type) {
+		console.log('downloading', addresses[type])
+		const html = await download(addresses[type])
+
+		const dataStructure = pageParser(html)
+
+		return dataStructure.map(item => {
+			return Object.assign(item, {
+				imageUrl: stripPrefix(item.imageUrl)
+			})
+		})
+	}), {
 		refreshEvery: ms('10 minutes'),
 		checkToSeeIfItemsNeedToBeRefreshedEvery: ms('1 minute'),
 		ttl: ms('1 week'),
 	})
-
-	async function downloadDataAndImages(type) {
-		const imageDownloader = imageDownloaders[type]
-
-		console.log('downloading', addresses[type])
-		const html = await got(addresses[type]).then(response => response.body)
-
-		const dataStructure = pageParser(html)
-
-		return pMap(dataStructure, async item => {
-			const filename = await imageDownloader(item.imageUrl)
-			const newItem = Object.assign(item, {
-				imageUrls: {
-					'1': path.join(imagePaths[type], '1', filename),
-					'2': path.join(imagePaths[type], '2', filename),
-				}
-			})
-
-			return newItem
-		}, { concurrency: 5 })
-	}
 
 	return denodeify(cache.get)
 }
--- server/index.js
+++ server/index.js
@@ -10,33 +10,16 @@ const mount = require('koa-mount')
 
 const tmpDir = require('os-tmpdir')()
 
-const createImageDownloader = require('./image-downloader')
 const createCache = require('./download-cache')
 
 const app = new Koa()
 
-const imageDirectories = {
-	game: path.join(tmpDir, 'susd-game-img'),
-	video: path.join(tmpDir, 'susd-video-img')
-}
-
-console.log('downloading images to', tmpDir)
-const imageUrlPrefix = 'https://www.shutupandsitdown.com/wp-content/uploads/'
-const imageDownloaders = {
-	game: createImageDownloader({ outputDirectory: imageDirectories.game, urlPrefix: imageUrlPrefix }),
-	video: createImageDownloader({ outputDirectory: imageDirectories.video, urlPrefix: imageUrlPrefix }),
-}
-
-const imagePaths = {
-	game: 'img/game',
-	video: 'img/video',
-}
-
-const getFromCache = createCache({ imagePaths, imageDownloaders })
+const getFromCache = createCache({
+	imageUrlPrefix: 'https://www.shutupandsitdown.com/wp-content/uploads/'
+})
 
 getFromCache('game')
 getFromCache('video')
-
 
 function susdDataMiddleware(dataType) {
 	return async function(context, next) {
@@ -50,14 +33,9 @@ function susdDataMiddleware(dataType) {
 router.get('/game', susdDataMiddleware('game'))
 router.get('/video', susdDataMiddleware('video'))
 
-router.get('/susd-image/:whatever(.*)')
-
 app.use(compress())
 
 app.use(router.routes())
-
-app.use(mount('/' + imagePaths.game, serve(imageDirectories.game)))
-app.use(mount('/' + imagePaths.video, serve(imageDirectories.video)))
 
 app.use(serve('./public/'))
 
