20b37fe62ade2f18f9c62fc0c441e003c5602dc1
Josh Duff <me@JoshDuff.com>
2016-11-27 21:41:00 -0600

    Implementing my own search algorithm that seems reasonable

--- client/search-data.js
+++ client/search-data.js
@@ -1,5 +1,3 @@
-const Fuse = require('fuse.js')
-
 function get(url) {
 	return new Promise((resolve, reject) => {
 		const request = new XMLHttpRequest()
@@ -13,33 +11,13 @@ function get(url) {
 
 
 module.exports = function searchData(source) {
-	const dataPromise = get(source).then(data => {
-		const dataWithTagsAsWords = data.map(data => {
-			return Object.assign(data, {
-				tagsAsWords: data.tags.map(tag => tag.replace('-', ' ')).join(' ')
-			})
-		})
-
-		const index = new Fuse(data, {
-			threshold: 0.5,
-			keys: [{
-				name: 'title',
-				weight: 0.8,
-			}, {
-				name: 'tagsAsWords',
-				weight: 0.2,
-			}]
-		})
-
-		return {
-			index,
-			data
-		}
-	})
+	const dataPromise = get(source)
 
 	return function search(searchString, filterTags) {
-		return dataPromise.then(({ data, index }) => {
-			const resultsBeforeTagFiltering = searchString ? index.search(searchString) : data
+		return dataPromise.then(data => {
+			const resultsBeforeTagFiltering = searchString
+				? findOrderedResults({ searchString, data })
+				: data
 
 			const results = filterTags.length === 0
 				? resultsBeforeTagFiltering
@@ -51,6 +29,52 @@ module.exports = function searchData(source) {
 			}
 		})
 	}
+}
+
+function findOrderedResults({ searchString, data }) {
+	const lowercaseSearchString = searchString.toLowerCase()
+	const searchTokens = tokenize(lowercaseSearchString)
+
+	return data.map(item => {
+		return {
+			item,
+			score: searchScore({ item, searchTokens, searchString: lowercaseSearchString })
+		}
+	})
+		.filter(({ score }) => score > 0)
+		.sort((a, b) => b.score - a.score)
+		.map(({ item }) => item)
+}
+
+function searchScore({ item, searchTokens, searchString }) {
+	return titleScore({ item, searchTokens, searchString })
+		+ tagScore({ tags: item.tags, searchTokens })
+}
+
+function titleScore({ item, searchTokens, searchString }) {
+	const lowercaseTitle = item.title.toLowerCase()
+
+	if (contains(lowercaseTitle, searchString)) {
+		return 100
+	} else {
+		const matchingTokens = searchTokens.filter(token => contains(lowercaseTitle, token)).length
+		return matchingTokens * 5
+	}
+}
+
+function tagScore({ tags, searchTokens }) {
+	return searchTokens.reduce((totalScore, token) => {
+		const matchesToken = tags.some(tag => contains(tag, token))
+		return totalScore + (matchesToken ? 1 : 0)
+	}, 0)
+}
+
+function contains(string, needle) {
+	return string.indexOf(needle) !== -1
+}
+
+function tokenize(searchString) {
+	return searchString.toLowerCase().split(/\s+/)
 }
 
 function containsAllTags({ items, filterTags }) {
