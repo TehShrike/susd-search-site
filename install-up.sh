6ab57bd6ab3b9e0b2d0f93f933137ad65d711e3d
Josh Duff <me@JoshDuff.com>
2019-03-01 11:57:53 -0600

    travis -> circle

--- install-up.sh
+++
@@ -1,3 +0,0 @@
-echo "sudo chown -R $(whoami) /usr/local/bin:" && sudo chown -R $(whoami) /usr/local/bin \
-&& echo "curl -sf https://up.apex.sh/install | sh:" && curl -sf https://up.apex.sh/install | sh \
-&& echo "up upgrade:" && up upgrade
